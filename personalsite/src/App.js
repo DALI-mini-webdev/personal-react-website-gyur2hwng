5f21b40c3b37dd10f35e1fb38a91d6ed5d39f18f
gyur2hwng <gyuri.hwang.23@dartmouth.edu>
2021-01-31 20:07:08 +0900

    react website revised

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,70 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /*interest variables*/
+  const interestList = [
+    {
+      id: "swe",
+      name: "Software Engineering"
+    },
+    {
+      id: "pm",
+      name: "Product Management"
+    },
+    {
+      id: "perfumes",
+      name: "Perfumes"
+    },
+    {
+      id: "playlists",
+      name: "Playlists"
+    },
+    {
+      id: "netflix",
+      name: "Netflix"
+    },
+    {
+      id: "vball",
+      name: "Volleyball"
+    },
+    {
+      id: "travel",
+      name: "Traveling"
+    },
+    {
+      id: "dogs",
+      name: "Dogs"
+    },
+    {
+      id: "sleep",
+      name: "Sleeping"
+    }
+  ]
+
+  const thumbsUpFunc = () => {
+    console.log("thumbsUp clicked");
+  }
+
+  const thumbsDownFunc = () => {
+    console.log("thumbsDown clicked");
+  }
+
+  const interestMap = interestList.map((interest) => {
+    return(
+      <ul key={interest.id}>{interest.name} <button onClick={thumbsUpFunc}>&#128077;</button><button onClick={thumbsDownFunc}>&#128078;</button></ul>
+    )
+    
+  });
+
+  const helloVisitor = (event) => {
+    <p>Hello {event.target.value}</p>
+  }
+
+  const keyDownFunction = (event) => {
+    if (event.key === 'Enter'){
+      console.log("Enter pressed");
+      return (helloVisitor)
+    }
+  }
+
   return (
     <div className="App">
-      <head>
-        <title>Gyuri's Website</title>
+       <header className="App-header">
+        <h1>Hi, I'm Gyuri!</h1>
+        <h2>Who are you?</h2>
+        <input type="text" onKeyDown={keyDownFunction}/>
+       </header>
 
-        /* favicon */
-        <link rel="icon" href="img/me_favicon.png" />
-
-        /* external stylesheet import */
-        <link rel="stylesheet" type="text/css" href="style.css" />
-      </head>
-        
-      <body>
-        /* menu on the top */
+       <div className="App-body">
+        {/* menu on the top */}
         <nav id="nav">
           <ul>
-              <li><a href="index.html">Home</a></li>
-              <li><a href="https://www.google.com">Past</a></li>
+              <li><a href="App.js">Home</a></li>
+              <li><a href="https://www.google.com">About Me</a></li>
               <li><a href="https://www.google.com">Present</a></li>
               <li><a href="https://www.google.com">Future</a></li>
           </ul>
         </nav>
     
-        <div id="contents">
-          /* My Photo */
-          <img id="mypic" src="img/me.jpg" width="360" height="480" />
-
-          /* Writing */
-          <h1>Hi, I'm Gyuri!</h1>
-          <p> I'm a '23 at Dartmouth majoring in Computer Science and trying to choose between
-          Music or Human Centered Design minor. On campus, I'm involved in Women in
-          Computer Science as an exec and Dartmouth College Wind Ensemble as a flutist. I often 
-          participate in flute projects as well, which is currently recording the Chaminade 
-          Flute Concerto Op. 107. I'm also a TA for CS10 this term.<br /><br />
-
-          I lived in Japan and China for four years each when I was little, but
-          afterwards grew up in Seoul, South Korea and came to New Hampshire
-          to go to Dartmouth! Currently I'm back at my home in Seoul. <br /><br />
-
-          Fun fact about me: my fingers are doublejointed!! <br /><br />
-          </p>
-          
-
-          <h2>Interests</h2>
-          <p>My interest is all over the place, to be honest, and some of them are
-            <ul>
-              <li>Software Engineering</li>
-              <li>Product Management</li>
-              <li>Awesome song playlists</li>
-              <li>Netflix</li>
-              <li>Perfumes/Fragrances</li>
-              <li>Volleyball</li>
-              <li>Traveling</li>
-              <li>Cuddling with my golden</li>
-              <li>Sleeping</li>
-            </ul>
+        <div id="intro">
+          {/* Writing */}
+          <h2>Sneak Peak of Me</h2>
+          <h3> Dartmouth '23 &#127794;</h3><br></br>
+          <p id="title">MAJOR: Computer Science <br></br>
+          MINOR/MOD: ?? (Music? HCD?)  ¯\_(ツ)_/¯ <br></br>
+          ON CAMPUS: WiCS exec, Dartmouth College Wind Ensemble flutist, CS10 TA <br></br>
+          MY ~HISTORY~: Tokyo, Japan  &#10145;  Guangzhou, China  &#10145;  Seoul, South Korea  &#10145;  Hanover, US  &#10145;  Seoul, South Korea <br ></br>
+          FUN FACT: My fingers are doublejointed!! <br></br>
           </p>
         </div>
 
-      </body>
+        <div id="interests">
+          <h2>Interests</h2>
+          <h3>My interests are all over the place, tbh, <br></br>
+          and some of them are</h3> 
+          <p id="comment">click on &#128077; if it's yours too! &#128078; if you're like meh</p>
+          {interestMap} 
+        </div>
+
+      </div>
     </div>
 
   );
+
 }
 
 export default App;
